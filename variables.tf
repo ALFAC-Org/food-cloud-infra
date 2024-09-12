21375db387ce59f9e112944e45954911656353fc
Leonardo Fraga <lafraga91@gmail.com>
2024-09-12 19:55:16 -0300

    Remove db files and add initial files for lambda and api gateway

--- variables.tf
+++ variables.tf
@@ -19,33 +19,12 @@ variable "subnet_public_cidr_block" {
   type        = string
 }
 
-variable "subnet_database_1_cidr_block" {
-  description = "CIDR block for the database subnet"
-  type        = string
-}
-
-variable "subnet_database_2_cidr_block" {
-  description = "CIDR block for the database subnet"
-  type        = string
-}
-
 variable "subnet_availability_zone_az_1" {
   description = "Availability zone for the subnets"
   type        = string
 }
 
-variable "subnet_availability_zone_az_2" {
-  description = "Availability zone 2 for the subnets"
+variable "arn_aws_lab_role" {
+  description = "ARN for the IAM role"
   type        = string
-}
-
-variable "db_username" {
-  description = "The username for the RDS instance"
-  type        = string
-}
-
-variable "db_password" {
-  description = "The password for the RDS instance"
-  type        = string
-  sensitive   = true
 }
