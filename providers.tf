c8d495190b5ce252c3f6824b0225e7599deed001
carlohcs <carlohcs@gmail.com>
2024-09-15 12:56:36 -0300

    feat: merge de remove-db-from-infra + ajustes para eks

--- providers.tf
+++ providers.tf
@@ -16,7 +16,7 @@ terraform {
 # We don't define the provider's credentials here because we are using the AWS CLI to authenticate.
 # https://registry.terraform.io/providers/hashicorp/aws/5.65.0/docs?utm_content=documentLink&utm_medium=Visual+Studio+Code&utm_source=terraform-ls#environment-variables
 provider "aws" {
-  region = var.region
+  region = var.aws_region
 }
 
 provider "kubernetes" {
