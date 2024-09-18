fba330d39b425c197c32d6d4768419773b81d4f8
carlohcs <carlohcs@gmail.com>
2024-09-18 18:34:24 -0300

    chore: adiciona config map e secret refs ao kubernetes

--- kubernetes.tf
+++ kubernetes.tf
@@ -45,6 +45,15 @@ resource "kubernetes_deployment" "deployment_food_app" {
             }
           }
 
+          env_from {
+            config_map_ref {
+              name = kubernetes_config_map.cm_food.metadata[0].name
+            }
+            secret_ref {
+              name = kubernetes_secret.secret_food.metadata[0].name
+            }
+          }
+
           port {
             container_port = var.app_port
           }
@@ -104,6 +113,37 @@ resource "kubernetes_ingress_v1" "food_app_ingress" {
         }
       }
     }
+  }
+}
+
+resource "kubernetes_secret" "secret_food" {
+  metadata {
+    name = "secret-food"
+  }
+
+  type = "Opaque"
+
+  data = {
+    APPLICATION_VERSION          = var.image_version
+    APPLICATION_DATABASE_VERSION = "latest"
+    MYSQL_ROOT_PASSWORD          = var.db_password
+    # TODO: Quando tivermos as configurações de banco, precisamos adaptar aqui
+    MYSQL_DATABASE               = "pedidos"
+    MYSQL_USER                   = var.db_password
+    MYSQL_PASSWORD               = var.db_password
+    SPRING_DATASOURCE_USERNAME   = var.db_password
+    SPRING_DATASOURCE_PASSWORD   = var.db_password
+  }
+}
+
+resource "kubernetes_config_map" "cm_food" {
+  metadata {
+    name = "cm-food"
+  }
+
+  # TODO: Quando tivermos as configurações de banco, precisamos adaptar aqui
+  data = {
+    SPRING_DATASOURCE_URL = "jdbc:mysql://svc-db-food:3306/pedidos"
   }
 }
 
