832f74755047df6494a80b6088e0771ab9e8243c
Leonardo Fraga <lafraga91@gmail.com>
2024-09-25 16:18:49 -0300

    api-gateway sekeleton

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,91 +1,111 @@
-# # Cria a API Gateway
-# resource "aws_api_gateway_rest_api" "rest_api" {
-#   name        = "authentication-api"
-#   description = "Authentication API Gateway"
-# }
+# Cria a API Gateway dentro da VPC
+resource "aws_api_gateway_rest_api" "rest_api" {
+  name        = "food_api_gateway"
+  description = "Food API Gateway"
+  endpoint_configuration {
+    types = ["PRIVATE"]
+    vpc_endpoint_ids = [aws_vpc_endpoint.api_gw_vpc_endpoint.id]
+  }
+}
 
-# # Cria o recurso do API Gateway
-# resource "aws_api_gateway_resource" "auth_resource" {
-#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-#   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-#   path_part   = "auth"
-# }
+# Cria o VPC Endpoint para o API Gateway
+resource "aws_vpc_endpoint" "api_gw_vpc_endpoint" {
+  vpc_id            = aws_vpc.food_vpc.id
+  service_name      = "com.amazonaws.${var.aws_region}.execute-api"
+  vpc_endpoint_type = "Interface"
+  subnet_ids        = [
+      aws_subnet.food_public_subnet_1.id,
+      aws_subnet.food_public_subnet_2.id,
+    ]
+  security_group_ids = [aws_security_group.api_gw_sg.id]
+}
 
-# https://food-api-gateway.execute-api.us-east-1.amazonaws.com/prod/auth?cpf=12345678900 
-# # Define o método do API Gateway para aceitar um parâmetro
-# resource "aws_api_gateway_method" "auth_method" {
-#   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-#   resource_id   = aws_api_gateway_resource.auth_resource.id
-#   http_method   = "POST"
-#   authorization = "NONE"
+# Cria o recurso do API Gateway
+resource "aws_api_gateway_resource" "auth_resource" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part   = "auth"
+}
 
-#   request_parameters = {
-#     "method.request.querystring.cpf" = true
-#   }
-# }
+# Cria o autorizer Lambda
+resource "aws_api_gateway_authorizer" "lambda_authorizer" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  name        = "lambda_authorizer"
+  type        = "TOKEN"
+  authorizer_uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.valida_cpf_usuario.arn}/invocations"
+  identity_source = "method.request.header.Authorization"
+}
 
-# # Define a integração do API Gateway para chamar o Cognito
-# resource "aws_api_gateway_integration" "auth_integration" {
-#   rest_api_id             = aws_api_gateway_rest_api.rest_api.id
-#   resource_id             = aws_api_gateway_resource.auth_resource.id
-#   http_method             = aws_api_gateway_method.auth_method.http_method
-#   type                    = "AWS"
-#   integration_http_method = "POST"
-#   uri                     = "arn:aws:apigateway:${var.region}:cognito-idp:action/AdminInitiateAuth"
+# Define o método do API Gateway para aceitar um parâmetro e usar o autorizer Lambda
+resource "aws_api_gateway_method" "auth_method" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.auth_resource.id
+  http_method   = "POST"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
 
-#   credentials = var.arn_aws_lab_role
+  request_parameters = {
+    "method.request.querystring.cpf" = true // TODO: [LAF] Check if this is correct
+  }
+}
 
-#   request_templates = {
-#     "application/json" = <<EOF
-#     {
-#         "AuthFlow": "CUSTOM_AUTH",
-#         "ClientId": "${aws_cognito_user_pool_client.user_pool_client.id}",
-#         "UserPoolId": "${aws_cognito_user_pool.user_pool.id}",
-#         "AuthParameters": {
-#             "USERNAME": "$input.params('cpf')",
-#             "PASSWORD": "password"
-#         }
-#     }
-#     EOF
-#   }
+# Define a integração do API Gateway para chamar o Load Balancer
+resource "aws_api_gateway_integration" "auth_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.auth_resource.id
+  http_method             = aws_api_gateway_method.auth_method.http_method
+  type                    = "HTTP_PROXY"
+  integration_http_method = "ANY"
+  uri                     = "http://${aws_lb.my_lb.dns_name}/"
+}
 
-#   request_parameters = {
-#     "integration.request.querystring.cpf" = "method.request.querystring.cpf"
-#   }
+# Cria o Load Balancer - TODO: apontar para o nosso LB ja criado
+resource "aws_lb" "my_lb" {
+  name               = "my-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = var.subnet_ids
+}
 
-#   passthrough_behavior = "WHEN_NO_MATCH"
-# }
+# Cria o grupo de segurança para o API Gateway - Precisamos de um grupo de segurança para o API Gateway?
+resource "aws_security_group" "api_gw_sg" {
+  name        = "api-gw-sg"
+  description = "Allow API Gateway access"
+  vpc_id      = aws_vpc.food_vpc.id
 
-# resource "aws_api_gateway_deployment" "deployment" {
-#   depends_on = [aws_api_gateway_integration.auth_integration]
-#   rest_api_id = aws_api_gateway_rest_api.rest_api.id
-#   stage_name  = "prod"
-# }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# # Cognito User Pool
-# resource "aws_cognito_user_pool" "user_pool" {
-#   name = "user_pool"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
-#   lambda_config {
-#     pre_authentication = aws_lambda_function.valida_cpf_usuario.arn
-#   }
+# Cria o grupo de segurança para o Load Balancer - TOD0: Ja devemos ter, validar se precisamos
+resource "aws_security_group" "lb_sg" {
+  name        = "lb-sg"
+  description = "Allow Load Balancer access"
+  vpc_id      = var.vpc_id
 
-#   auto_verified_attributes = ["email"] #TODO: Validar se precisamos disso
-# }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-# # Cognito User Pool Client
-# resource "aws_cognito_user_pool_client" "user_pool_client" {
-#   name         = "user_pool_client"
-#   user_pool_id = aws_cognito_user_pool.user_pool.id
-
-#   explicit_auth_flows = ["CUSTOM_AUTH_FLOW_ONLY"]
-# }
-
-# # Permissão para a função Lambda ser invocada pelo Cognito
-# resource "aws_lambda_permission" "allow_cognito_invoke" {
-#   statement_id  = "AllowCognitoInvoke"
-#   action        = "lambda:InvokeFunction"
-#   function_name = aws_lambda_function.valida_cpf_usuario.function_name
-#   principal     = "cognito-idp.amazonaws.com"
-#   source_arn    = aws_cognito_user_pool.user_pool.arn
-# }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
