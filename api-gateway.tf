ebd5592e830d760f349467b63f0b225762b23572
Leonardo Fraga <lafraga91@gmail.com>
2024-09-25 22:40:58 -0300

    Add output

--- api-gateway.tf
+++ api-gateway.tf
@@ -49,7 +49,7 @@ resource "aws_apigatewayv2_vpc_link" "vpc_link" {
 resource "aws_apigatewayv2_integration" "auth_integration" {
   api_id             = aws_apigatewayv2_api.http_api.id
   integration_type   = "HTTP_PROXY"
-  integration_uri    = data.aws_lb_listener.food_lb_listener.arn
+  integration_uri    = "net/ae2c9f587f7284a278cdd7059897eb90/210a87acaa33d1a8/c545794835f5c70c"
   integration_method = "ANY"
   connection_type    = "VPC_LINK"
   connection_id      = aws_apigatewayv2_vpc_link.vpc_link.id
