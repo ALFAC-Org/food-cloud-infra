411cf14463f4d75276eeba80f7c384cb0261da10
Leonardo Fraga <lafraga91@gmail.com>
2024-09-26 20:36:15 -0300

    Update api-gateway

--- api-gateway.tf
+++ api-gateway.tf
@@ -47,17 +47,17 @@ resource "aws_apigatewayv2_authorizer" "lambda_authorizer" {
 }
 
 # Define a rota do API Gateway para aceitar todas as requisições que começam com /pedidos e usar o autorizer Lambda
-resource "aws_apigatewayv2_route" "auth_route" {
-  api_id = aws_apigatewayv2_api.http_api.id
-  route_key          = "ANY /{proxy+}"
-  authorization_type = "CUSTOM"
-  authorizer_id      = aws_apigatewayv2_authorizer.lambda_authorizer.id
-  target             = "integrations/${aws_apigatewayv2_integration.auth_integration.id}"
+# resource "aws_apigatewayv2_route" "auth_route" {
+#   api_id = aws_apigatewayv2_api.http_api.id
+#   route_key          = "ANY /{proxy+}"
+#   authorization_type = "CUSTOM"
+#   authorizer_id      = aws_apigatewayv2_authorizer.lambda_authorizer.id
+#   target             = "integrations/${aws_apigatewayv2_integration.auth_integration.id}"
 
-  lifecycle {
-    prevent_destroy = false
-  }
-}
+#   lifecycle {
+#     prevent_destroy = false
+#   }
+# }
 
 # Cria o VPC Link para a integração com o Load Balancer
 resource "aws_apigatewayv2_vpc_link" "vpc_link" {
