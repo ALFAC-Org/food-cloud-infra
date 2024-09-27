cbd5c24547088297d9e6c30fda769066b3f103ab
Leonardo Fraga <lafraga91@gmail.com>
2024-09-26 21:58:59 -0300

    ARN

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,13 +1,17 @@
-# # Declara o recurso aws_lb para o Load Balancer usando o nome
-# data "aws_lb" "food_lb" {
-#   name = "nlb-food-app" # Substitua pelo nome real do seu Load Balancer
-# }
+# Declara o recurso aws_lb para o Load Balancer usando o nome
+data "aws_lb" "food_lb" {
+  name = "a0afc84dbb3b5404fa366a4bf89e3a36" # Substitua pelo nome real do seu Load Balancer
+}
 
-# # Declara o recurso aws_lb_listener para obter o ARN do listener
-# data "aws_lb_listener" "food_lb_listener" {
-#   load_balancer_arn = data.aws_lb.food_lb.arn
-#   port              = 8080
-# }
+# Declara o recurso aws_lb_listener para obter o ARN do listener
+data "aws_lb_listener" "food_lb_listener" {
+  load_balancer_arn = data.aws_lb.food_lb.arn
+  port              = 8080
+}
+
+output "teste_Fraga" {
+  value = data.aws_lb_listener.food_lb_listener.arn
+}
 
 # Cria a API Gateway do tipo HTTP API
 resource "aws_apigatewayv2_api" "http_api" {
@@ -80,8 +84,7 @@ resource "aws_apigatewayv2_vpc_link" "vpc_link" {
 resource "aws_apigatewayv2_integration" "auth_integration" {
   api_id             = aws_apigatewayv2_api.http_api.id
   integration_type   = "HTTP_PROXY"
-  #integration_uri    = data.aws_lb_listener.food_lb_listener.arn
-  integration_uri    = "arn:aws:elasticloadbalancing:us-east-1:819532756232:loadbalancer/net/a0afc84dbb3b5404fa366a4bf89e3a36/c6574c0f69d4dfd6"
+  integration_uri    = data.aws_lb_listener.food_lb_listener.arn
   integration_method = "ANY"
   connection_type    = "VPC_LINK"
   connection_id      = aws_apigatewayv2_vpc_link.vpc_link.id
