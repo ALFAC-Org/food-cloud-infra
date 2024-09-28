ca766365a054825eb311b0d3f54e5a2a76d3792d
carlohcs <carlohcs@gmail.com>
2024-09-28 01:37:59 -0300

    chore: config para trigger de lambda pelo gateway

--- api-gateway.tf
+++ api-gateway.tf
@@ -4,7 +4,7 @@ data "aws_lb" "food_lb" {
     "kubernetes.io/service-name" = "default/service-food-app"
   }
 
-  depends_on = [ kubernetes_service.food_app_service ]
+  depends_on = [kubernetes_service.food_app_service]
 }
 
 # Declara o recurso aws_lb_listener para obter o ARN do listener
@@ -12,7 +12,7 @@ data "aws_lb_listener" "food_lb_listener" {
   load_balancer_arn = data.aws_lb.food_lb.arn
   port              = 8080
 
-  depends_on = [ kubernetes_service.food_app_service ]
+  depends_on = [kubernetes_service.food_app_service]
 }
 
 # output "teste_Fraga" {
@@ -58,7 +58,7 @@ resource "aws_apigatewayv2_authorizer" "lambda_authorizer" {
 
 # Define a rota do API Gateway para aceitar todas as requisições que começam com /pedidos e usar o autorizer Lambda
 resource "aws_apigatewayv2_route" "auth_route" {
-  api_id = aws_apigatewayv2_api.http_api.id
+  api_id             = aws_apigatewayv2_api.http_api.id
   route_key          = "ANY /{proxy+}"
   authorization_type = "CUSTOM"
   authorizer_id      = aws_apigatewayv2_authorizer.lambda_authorizer.id
@@ -68,7 +68,7 @@ resource "aws_apigatewayv2_route" "auth_route" {
     prevent_destroy = false
   }
 
-  depends_on = [ aws_apigatewayv2_integration.auth_integration ]
+  depends_on = [aws_apigatewayv2_integration.auth_integration]
 }
 
 # Cria o VPC Link para a integração com o Load Balancer
@@ -81,7 +81,7 @@ resource "aws_apigatewayv2_vpc_link" "vpc_link" {
   security_group_ids = [
     aws_security_group.api_gw_sg.id,
     aws_security_group.eks_security_group.id,
-]
+  ]
 
   lifecycle {
     prevent_destroy = false
@@ -89,22 +89,21 @@ resource "aws_apigatewayv2_vpc_link" "vpc_link" {
 }
 
 # Define a integração do API Gateway para chamar a função Lambda
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id           = aws_apigatewayv2_api.http_api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.valida_cpf_usuario.arn}/invocations"
-  payload_format_version = "2.0"
+# resource "aws_apigatewayv2_integration" "lambda_integration" {
+#   api_id           = aws_apigatewayv2_api.http_api.id
+#   integration_type = "AWS_PROXY"
+#   integration_uri  = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.valida_cpf_usuario.arn}/invocations"
+#   payload_format_version = "2.0"
 
-  lifecycle {
-    prevent_destroy = false
-  }
-}
+#   lifecycle {
+#     prevent_destroy = false
+#   }
+# }
 
 # Define a integração do API Gateway para chamar o Load Balancer
 resource "aws_apigatewayv2_integration" "auth_integration" {
   api_id             = aws_apigatewayv2_api.http_api.id
-  #integration_type   = "HTTP_PROXY"
-   integration_type = "AWS_PROXY"
+  integration_type   = "HTTP_PROXY"
   integration_uri    = data.aws_lb_listener.food_lb_listener.arn
   integration_method = "ANY"
   connection_type    = "VPC_LINK"
@@ -121,7 +120,7 @@ resource "aws_apigatewayv2_integration" "auth_integration" {
     prevent_destroy = false
   }
 
-  depends_on = [ kubernetes_service.food_app_service ]
+  depends_on = [kubernetes_service.food_app_service]
 }
 
 # Permissão para a API Gateway invocar a função Lambda
